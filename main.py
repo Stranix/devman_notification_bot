532fe676650ee0ee8bb79aa07f775a8b3c0bec46
Stranix <Phantom2525@gmail.com>
2023-07-03 23:02:34 +0300

    refactor code
    create new func long_polling
    use app settings

--- main.py
+++ main.py
@@ -1,59 +1,15 @@
-import os
 import sys
 
-from pprint import pprint
-from dotenv import load_dotenv
-
-import services
-
-load_dotenv()
+from services import start_polling
 
 
 def main():
     try:
-        devman_token = os.environ['DEVMAN_TOKEN']
-        tg_bot_token = os.environ['TG_BOT_TOKEN']
-        tg_recipient_chat_id = os.environ['RECIPIENT_CHAT_ID']
-        url = 'https://dvmn.org/api/long_polling/'
-        params = {}
-        while True:
-            response = services.send_long_pooling_request(
-                url=url,
-                token=devman_token,
-                params=params,
-            )
-            devman_api = response.json()
-
-            if devman_api['status'] == 'timeout':
-                params['timestamp'] = devman_api['timestamp_to_request']
-                print('Задан параметр timestamp', devman_api['timestamp_to_request'])
-
-            if devman_api['status'] == 'found':
-                params['timestamp'] = devman_api['last_attempt_timestamp']
-                pprint(devman_api)
-                lesson_title = devman_api['new_attempts'][0]['lesson_title']
-                lesson_url = devman_api['new_attempts'][0]['lesson_url']
-
-                notification_message = f'У вас проверили работу\n<code>{lesson_title}</code>\n\n'
-                if devman_api['new_attempts'][0]['is_negative']:
-                    notification_message += 'В работе нашлись ошибки\n'
-                else:
-                    notification_message += 'Работа принята!\n'
-                notification_message += lesson_url
-
-                services.tg_bot_send_message(
-                    tg_bot_token,
-                    int(tg_recipient_chat_id),
-                    notification_message,
-                )
-    except KeyError:
-        print('Заданы не все переменные окружения.')
+        start_polling()
     except KeyboardInterrupt:
         print('Работа остановлена')
-    finally:
         sys.exit()
 
 
 if __name__ == '__main__':
     main()
-
--- services.py
+++ services.py
@@ -1,54 +1,86 @@
 import time
 import requests
 
+from urllib.parse import urljoin
 
-def send_long_pooling_request(
-        url: str,
-        token: str,
-        params: dict,
-        timeout=95
-) -> requests.Response:
-    headers = {
-        'Authorization': 'Token {}'.format(token),
-    }
+from config import settings
+
+
+def create_notification_message(attempt: dict) -> str:
+    lesson_title = attempt['lesson_title']
+    lesson_url = attempt['lesson_url']
+
+    message = f'У вас проверили работу\n<code>{lesson_title}</code>\n\n'
+    if attempt['is_negative']:
+        message += 'В работе нашлись ошибки\n'
+    else:
+        message += 'Работа принята!\n'
+    message += lesson_url
+    return message
+
+
+def start_polling():
+    params = {}
     while True:
         try:
-            print('Отправка запроса на', url)
-            response = requests.get(
-                url,
-                params=params,
-                headers=headers,
-                timeout=timeout
-            )
+            response = send_request(params)
             response.raise_for_status()
-            print('Выполнен запрос на ', response.url)
-            return response
+            devman_api = response.json()
+
+            if devman_api['status'] == 'timeout':
+                params['timestamp'] = devman_api['timestamp_to_request']
+                print('Задан параметр timestamp',
+                      devman_api['timestamp_to_request'])
+
+            if devman_api['status'] == 'found':
+                params['timestamp'] = devman_api['last_attempt_timestamp']
+                for attempt in devman_api['new_attempts']:
+                    notification_message = create_notification_message(attempt)
+                    tg_bot_send_message(notification_message)
         except requests.exceptions.HTTPError:
             print('Ошибка HTTP. Код ответа не 200')
         except requests.exceptions.ReadTimeout:
             print('Тайм-аут ожидания ответа от сервера')
         except requests.exceptions.ConnectionError:
             print(
-                'Нет интернета. Попробуем отправить повторный запрос через '
+                'Нет соединения. Попробуем отправить повторный запрос через '
                 '5 сек'
             )
             time.sleep(5)
 
 
+def send_request(params: dict, timeout=95) -> requests.Response:
+    url = urljoin(settings.devman_api_url, 'long_polling/')
+    print('Отправка запроса на', url, 'с параметрами', params)
+    headers = {
+        'Authorization': 'Token {}'.format(settings.devman_token),
+    }
+
+    response = requests.get(
+        url,
+        params=params,
+        headers=headers,
+        timeout=timeout
+    )
+    response.raise_for_status()
+    print('Запрос выполнен')
+    return response
+
+
 def tg_bot_send_message(
-        tg_bot_token: str,
-        chat_id: int,
         message: str,
         parse_mode: str = 'HTML'
 ):
-    tg_url = 'https://api.telegram.org/bot{}/sendMessage'.format(tg_bot_token)
+    print('Отправка сообщения через телеграм бота')
+    tg_bot_url = urljoin(settings.tg_api_url, f'/bot{settings.tg_bot_token}')
+    send_message_url = tg_bot_url + '/sendMessage'
     params = {
-        'chat_id': chat_id,
+        'chat_id': settings.tg_recipient_chat_id,
         'text': message,
         'parse_mode': parse_mode,
     }
 
-    response = requests.get(tg_url, params=params)
+    response = requests.get(send_message_url, params=params)
     tg_api = response.json()
     print(tg_api)
     if not tg_api['ok']:
