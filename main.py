07c4ecf50ca8e08bf6a9252e1d23157c99e9ca07
Stranix <Phantom2525@gmail.com>
2023-07-03 20:19:55 +0300

    add tg bot send custom message

--- main.py
+++ main.py
@@ -31,10 +31,20 @@ def main():
             if devman_api['status'] == 'found':
                 params['timestamp'] = devman_api['last_attempt_timestamp']
                 pprint(devman_api)
+                lesson_title = devman_api['new_attempts'][0]['lesson_title']
+                lesson_url = devman_api['new_attempts'][0]['lesson_url']
+
+                notification_message = f'У вас проверили работу\n<code>{lesson_title}</code>\n\n'
+                if devman_api['new_attempts'][0]['is_negative']:
+                    notification_message += 'В работе нашлись ошибки\n'
+                else:
+                    notification_message += 'Работа принята!\n'
+                notification_message += lesson_url
+
                 services.tg_bot_send_message(
                     tg_bot_token,
                     int(tg_recipient_chat_id),
-                    'Преподаватель проверил работу',
+                    notification_message,
                 )
     except KeyError:
         print('Заданы не все переменные окружения.')
