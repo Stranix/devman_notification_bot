3449c2254d519365849a35abe8bbbc6e480d2717
Stranix <Phantom2525@gmail.com>
2023-07-04 13:56:27 +0300

    add docstring in func

--- services.py
+++ services.py
@@ -1,12 +1,20 @@
 import time
+
 import requests
 
+from typing import Any
 from urllib.parse import urljoin
 
 from config import settings
 
 
 def create_notification_message(attempt: dict) -> str:
+    """Создаем информационное сообщение для отправки в телеграм чат.
+
+    :param attempt: dict - словарь с информацией о проверенном уроке.
+
+    :return: str - информационное сообщение.
+    """
     lesson_title = attempt['lesson_title']
     lesson_url = attempt['lesson_url']
 
@@ -19,11 +27,18 @@ def create_notification_message(attempt: dict) -> str:
     return message
 
 
-def start_polling():
+def start_polling(timeout: int = 95):
+    """Запускаем долгие запросы на Devman API.
+
+    В случаи обнаружения проверенной работы будет отправлено уведомление
+    в телеграм чат.
+
+    :param timeout: int - время ожидания ответа от сервера. По умолчанию 95 сек
+    """
     params = {}
     while True:
         try:
-            response = send_request(params)
+            response = send_request(params, timeout)
             response.raise_for_status()
             devman_api = response.json()
 
@@ -49,7 +64,19 @@ def start_polling():
             time.sleep(5)
 
 
-def send_request(params: dict, timeout=95) -> requests.Response:
+def send_request(params: dict, timeout: Any = None) -> requests.Response:
+    """Отправляем запросы на указанный url.
+
+    Настройки url берутся из переменных окружения.
+
+    В случаи обнаружения проверенной работы будет отправлено уведомление
+    в телеграм чат.
+
+    :param params: параметры запроса в виде словаря.
+    :param timeout: время ожидания ответа от сервера. По умолчанию None.
+
+    :return: requests.Response - объект успешного ответа.
+    """
     url = urljoin(settings.devman_api_url, 'long_polling/')
     print('Отправка запроса на', url, 'с параметрами', params)
     headers = {
@@ -71,6 +98,14 @@ def tg_bot_send_message(
         message: str,
         parse_mode: str = 'HTML'
 ):
+    """Отправляем сообщение через телеграм бота в чат.
+
+    Настройки chat_id берется из параметров скрипта при старте.
+
+    :param message: str - сообщение для отправки.
+    :param parse_mode: str - режим парсинга сообщения со стороны телеграм. По умолчанию 'HTML'.
+
+    """
     print('Отправка сообщения через телеграм бота')
     tg_bot_url = urljoin(settings.tg_api_url, f'/bot{settings.tg_bot_token}')
     send_message_url = tg_bot_url + '/sendMessage'
@@ -82,6 +117,6 @@ def tg_bot_send_message(
 
     response = requests.get(send_message_url, params=params)
     tg_api = response.json()
-    print(tg_api)
+
     if not tg_api['ok']:
         raise requests.exceptions.HTTPError
