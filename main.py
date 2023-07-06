c51e2f88c60ed1f709937899cbf2dab6af231dca
Stranix <Phantom2525@gmail.com>
2023-07-06 19:02:59 +0300

    move settings from config to main

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
-import sys
+import os
+import argparse
+
+from dotenv import load_dotenv
 
 from services import start_polling
 
 
+def create_arg_parser():
+    description = 'Отправляем уведомления о проверке уроков devman в telegram'
+
+    arg_parser = argparse.ArgumentParser(
+        description=description,
+    )
+
+    arg_parser.add_argument('chat_id', metavar='', type=int,
+                            help='''id телеграм чата куда бот будет отправлять
+                            уведомления.
+                            '''
+                            )
+
+    return arg_parser
+
+
 def main():
     try:
-        start_polling()
+        load_dotenv()
+        args = create_arg_parser().parse_args()
+        devman_token = os.environ['DEVMAN_TOKEN']
+        tg_bot_token = os.environ['TG_BOT_TOKEN']
+        tg_recipient_chat_id = args.chat_id
+        start_polling(devman_token, tg_bot_token, tg_recipient_chat_id)
+    except KeyError:
+        print('Заданы не все настройки переменных окружения.')
     except KeyboardInterrupt:
         print('Работа остановлена')
-        sys.exit()
 
 
 if __name__ == '__main__':
